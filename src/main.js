d945a4d0653f4d128b04dd0e2c216e603cf1b45e
evangalen <emil.van.galen@gmail.com>
2021-04-22 15:34:18 +0200

    Removed unwanted comments in `main.js`

--- src/main.js
+++ src/main.js
@@ -10,13 +10,9 @@ const routes = [
     { path: '/done', component: DoneItemsPage },
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHashHistory(),
-    routes, // short for `routes: routes`
+    routes,
 })
 
 const app = createApp(App);
